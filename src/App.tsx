155f31fb6ea6c165ef0cebe637506bc1397d49c7
Nam Yooseong <nys8765@naver.com>
2026-01-28 19:11:06 +0900

    chore: clsx, tailwind-merge, class-variance-authority 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import viteLogo from "/vite.svg";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { cn } from "./utils";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const isHighlight = count > 0 && count % 5 === 0;
 
   return (
     <>
@@ -21,12 +24,17 @@ function App() {
         <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+      <p
+        className={cn(
+          "mt-4 text-sm",
+          isHighlight ? "text-orange-600 font-bold" : "text-gray-500",
+        )}
+      >
+        {isHighlight
+          ? "🎉 5의 배수 달성! 스타일이 변경되었습니다."
+          : "5의 배수가 되면 버튼이 변해요!"}
       </p>
     </>
   );
