abac0aa4b7fd76a09ebe894d38356af476e9f53d
Nam Yooseong <nys8765@naver.com>
2026-02-08 02:17:43 +0900

    refactor: 주석 제거, es6 객체 속성 축약 문법 적용

--- src/shared/utils/auth.ts
+++ src/shared/utils/auth.ts
@@ -1,6 +1,5 @@
 const SESSION_KEY = "moyeorak.session";
 
-// 브라우저에 저장된 UUID를 가져오거나 없으면 새로 생성하여 저장
 export function getGuestId(): string {
   let guestId = localStorage.getItem(SESSION_KEY);
 
@@ -12,12 +11,9 @@ export function getGuestId(): string {
   return guestId;
 }
 
-// 특정 모임별 정보 저장
-// @param meetingId 생성된 모임의 고유 ID
-// @param name 해당 모임에서 사용할 사용자 이름
 export function setMeetingSession(meetingId: string, name: string): void {
   const key = `moyeorak.session.meetings.${meetingId}`;
-  const data = { name: name };
+  const data = { name };
 
   localStorage.setItem(key, JSON.stringify(data));
 }
