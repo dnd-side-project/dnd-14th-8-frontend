a5ad574b7ed60fae77cb28089248603d6e184aba
sudosubin <sudosubin@gmail.com>
2026-02-16 20:34:02 +0900

    feat: React Router 기본 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import "@/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/App.tsx";
+import { ReactRouterProvider } from "@/shared/providers/react-router-provider";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <ReactRouterProvider>
+        <App />
+      </ReactRouterProvider>
     </StrictMode>,
   );
 }
---
+++ src/shared/pages/index/index.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { MobileLayout } from "@/shared/components/mobile-layout";
+import { cn } from "@/shared/utils/cn";
+
+export function IndexPage() {
+  const [count, setCount] = useState(0);
+  const isHighlight = count > 0 && count % 5 === 0;
+
+  return (
+    <MobileLayout>
+      <h1 className="text-h1 text-primary-main">Vite + React</h1>
+      <button
+        className="text-t2"
+        type="button"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        count is {count}
+      </button>
+
+      <p
+        className={cn(
+          "bg-amber-200",
+          isHighlight ? "text-b3 text-sb-700" : "text-b2 text-p-500",
+        )}
+      >
+        {isHighlight
+          ? "🎉 5의 배수 달성! 스타일이 변경되었습니다."
+          : "5의 배수가 되면 버튼이 변해요!"}
+      </p>
+    </MobileLayout>
+  );
+}
